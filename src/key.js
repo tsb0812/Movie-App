2536e260264f9ccf44a5c35aea6184674c077cbc
tusharsbauskar <tsbauskar0812@gmail.com>
2020-12-15 12:02:49 +0530

    TMDB API configuration

---
+++ src/key.js
@@ -0,0 +1,3 @@
+const API_KEY = "0d2c0168a371897ac17355f54266659a";
+
+export default API_KEY;
---
+++ src/requests.js
@@ -0,0 +1,7 @@
+import API_KEY from "./key";
+
+const requests = {
+  popular: `/movie/popular?api_key=${API_KEY}`,
+};
+
+export default requests;
